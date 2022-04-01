37a6569c23ce8da843e32c128ab23fd4fa5ac60a
rober <rober@HP-ZBOOK-ISOLAT>
2022-04-01 21:07:44 +0200

    Add project files.

---
+++ fence.cpp
@@ -0,0 +1,8 @@
+#include "fence.h"
+
+
+int fence::get_wire_length()
+{
+    // TODO: Add your implementation code here.
+    return wire_length;
+}
---
+++ fence.h
@@ -0,0 +1,11 @@
+#pragma once
+class fence
+{
+public:
+	int get_wire_length();
+	
+	int a;
+	int b;
+	int wire_length;
+};
+
