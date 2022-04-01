6e94586fb0ad3f8a00d5f0be5c8133c921c0d06c
dorota <dorota@HP-ZBOOK-ISOLAT>
2022-04-02 00:35:09 +0200

    Basic Class definition (Farm)

--- UncleMcDonald.cpp
+++ UncleMcDonald.cpp
@@ -4,6 +4,29 @@
 
 using namespace std;
 
+class Farm {
+private:
+    int a = 0, b = 0, area = 0;
+public:
+    void fenceLen(int wire_len) {
+        /*
+        the formula used to calculte the optimal dimensions is best explained here: https://blog.etrapez.pl/pochodne/zadania-optymalizacyjne/ (in polish)
+        or here in form of video: https://www.youtube.com/watch?v=lx8RcYcYVuU&t=1621s (in english)
+        since the floating part of the division will be discarded it should not cause a situtation where Uncle will not have enough wire to build a suggested size of fence
+        */
+        a = wire_len / 4;
+        b = wire_len - 2 * a;
+        area = a * b;
+    }
+    void printResult() {
+        //simple ASCII drawing, designed using https://asciiflow.com/
+        cout << "|   ++\n|   ||  a=?\n|   |+------+\n| W ||  F   |\n| A ||  a   |\n| L ||  r   |b=?\n| L ||  m   |\n|   ||      |\n|   |+------+\n|   ||  a=?\n|   ++\n";
+        //after showing a simple ASCII-art drawing, the resulting values are pritned
+        cout << "The most optimal dimensions, maximizing the area of your farm are as follows\n >>a: " << a << " meters,\n >>b: " << b << " meters.\nThis gives an area of: " << area << " square meters." << endl;
+    }
+};
+
+
 int getWireLen() {
     int wire_length = 0;
     while (wire_length <= 0) {
@@ -47,7 +70,10 @@ void farmingStarter() {
         if (input == "RUN") {
             cout << "Let's go!" << endl;
             //when user entered RUN, there will first be a prompt for wire length by getWireLen() function, this will be passed to getResult() function which calculates and prints the result
-            getResult(getWireLen());
+            //getResult(getWireLen());
+            Farm uncleFarm1;
+            uncleFarm1.fenceLen(getWireLen());
+            uncleFarm1.printResult();
             //since the input value triggering calculations is not 'EXTI', user will be presented with option to run the algorithm once again or exit the application.
         }
         else {
